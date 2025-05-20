e03bed0479c0e7e128269cd8f292ede3270b3fbb
season malla <season.malla@maitriservices.com>
2025-05-20 22:41:47 +0545

    nepse added

--- app.py
+++ app.py
@@ -6,15 +6,25 @@ app = Flask(__name__)
 nepse = Nepse()
 
 date='2025-05-19'
+nepse.setTLSVerification(False)
 
 @app.route('/')
 def hello():
     return 'Hello, World! apple'
 
-@app.route('/nepse')
-def prieVolumeHistory():
-    return nepse.getPriceVolumeHistory(date)
+@app.route('/m')
+def priceVolumeHistory():
+    print("start nepse")
+    data = nepse.getMarketStatus()
+    print(data)
+    return data
 
+@app.route('/a')
+def getSummarysss():
+    print("start nepse")
+    data = nepse.getSummary()
+    print(data)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
