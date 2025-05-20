bb03e605e94c2c0c35f8847de52dd2fe65bfa905
season malla <season.malla@maitriservices.com>
2025-05-20 22:13:28 +0545

    nepse route added

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from nepse import Nepse
 
 app = Flask(__name__)
+
+nepse = Nepse()
+
+date='2025-05-19'
 
 @app.route('/')
 def hello():
     return 'Hello, World! apple'
+
+@app.route('/nepse')
+def prieVolumeHistory():
+    return nepse.getPriceVolumeHistory(date)
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
